a1cfd78373ccd3e00d23632d44d04f3ff9c0941b
gick <gicquel.pierreyves@gmail.com>
2018-10-30 17:02:19 +0100

    mlg refactored

--- app/route/documentroutes.js
+++ app/route/documentroutes.js
@@ -27,6 +27,7 @@ module.exports = function (app, gfs, logger) {
 	var poiHandler=require('./resourcesRoutes/poiRoute.js')(app,logger)
 	var youtubeHandler=require('./resourcesRoutes/youtubeRoute.js')(app,logger)
 	var unitGameHandler=require('./resourcesRoutes/unitgameRoute.js')(app,logger)
+	var mlgHandler=require('./resourcesRoutes/mlgRoute.js')(app,logger)
 
 	
 
@@ -366,127 +367,6 @@ module.exports = function (app, gfs, logger) {
 	// Handle reception of a new free text activity designed by conceptor
 
 
-
-	app.post('/mlg', function (req, res) {
-		var mlg = new MLG()
-
-		mlg.label = req.body.label
-		mlg.startpage = req.body.startpage
-		mlg.endPage = req.body.endPage
-		mlg.owner = req.user._id
-		mlg.status = req.body.status
-		mlg.difficulty = req.body.difficulty
-		mlg.duration = req.body.duration
-		mlg.unitGames = req.body.situatedunitgame
-		mlg.description = req.body.description
-		mlg.badge = req.body.badge
-		var now = new Date()
-		mlg.creationDate = now
-		if (!req.body.itemId) {
-			mlg.save(function (err) {
-				if (err) {
-					logger.log('error', 'Error while saving MLG %s', err.message)
-					return 500
-				}
-
-				res.send({
-					success: true,
-					resource: mlg,
-					operation: 'create'
-				})
-			})
-		}
-
-		if (req.body.itemId && req.body.itemId.length > 0) {
-			MLG.findById(req.body.itemId, function (err, toUpdate) {
-				if (!toUpdate) {
-					logger.log('error', 'Err, MLG with id ' + req.body.itemId + ' does not exists')
-				} else {
-					logger.log('info', 'Updating MLG %s', JSON.stringify(toUpdate))
-					toUpdate.label = req.body.label
-					toUpdate.startpage = req.body.startpage
-					toUpdate.endPage = req.body.endPage
-					toUpdate.owner = req.user._id
-					toUpdate.status = req.body.status
-					toUpdate.difficulty = req.body.difficulty
-					toUpdate.duration = req.body.duration
-					toUpdate.unitGames = req.body.situatedunitgame
-					toUpdate.description = req.body.description
-					toUpdate.badge = req.body.badge
-					toUpdate.save(function (err) {
-						if (err) {
-							logger.log('error', 'Error while updating MLG %s', err.message)
-							res.send({
-								success: false
-							})
-						} else res.send({
-							success: true,
-							resource: toUpdate,
-							operation: 'update'
-						})
-
-					})
-
-				}
-
-			})
-		}
-	})
-
-
-
-
-
-
-
-
-
-	//handle reception lgof a complete game
-	app.get('/mlg', function (req, res) {
-		if (!req.isAuthenticated()) {
-			res.send({
-				success: false,
-				message: 'Please authenticate'
-			})
-			return
-		}
-
-		// MLG.find({ owner: req.user._id })
-		MLG.find({
-				$or: [{
-					owner: req.user._id
-				}, {
-					status: 'Public'
-				}]
-			})
-			.sort({
-				creationDate: -1
-			})
-			.deepPopulate(['startpage','endPage', 'badge', 'unitGames', 'unitGames.startMedia', 'unitGames.feedbackMedia', 'unitGames.freetextActivities', 'unitGames.mcqActivities', 'unitGames.mcqActivities.media', 'unitGames.inventoryItem', 'unitGames.inventoryItem.media', 'unitGames.inventoryItem.inventoryDoc', 'unitGames.POI'])
-			.exec(function (err, mlgs) {
-				for (var i = 0; i < mlgs.length; i++) {
-					var mlg = mlgs[i]
-					if (mlg.owner == req.user._id) {
-						mlg.readonly = 'readwrite'
-					} else {
-						mlg.readonly = 'readonly'
-					}
-				}
-				res.send(mlgs)
-			})
-	})
-
-	app.put('/mlg/:id/share', function (req, res) {
-		if (!req.isAuthenticated()) {
-			res.send({
-				success: false,
-				message: 'Please authenticate'
-			})
-			return
-		}
-		switchStatus(MLG, req, res)
-
-	})
 
 
 
@@ -865,20 +745,6 @@ module.exports = function (app, gfs, logger) {
 		})
 	}
 
-	app.delete('/mlg/:id', function (req, res) {
-		MLG.findOneAndRemove({
-				'_id': req.params.id,
-				owner: req.user._id
-			},
-			function (err, doc) {
-				res.send({
-					success: true,
-					resource: doc,
-					operation: 'delete'
-				})
-			})
-
-	})
 	//self explaining
 
 
